6eca709f6007408c82872c8c9467a6678ee98bba
koushikdevnath3 <45910579+koushikdevnath3@users.noreply.github.com>
2024-09-12 18:22:00 +0530

    update
    test

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   sassOptions: {
     includePaths: ['./scss'], // Adjust the path if your SCSS files are in ./scss
   },
 }
-
 module.exports = nextConfig

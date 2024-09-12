9fc1bcd66e6229b66c612ee5c1977060d7c4e3b9
koushikdevnath3 <45910579+koushikdevnath3@users.noreply.github.com>
2024-09-12 18:04:40 +0530

    test
    test 1.2

--- pages/500.js
+++ pages/500.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Custom500 = () => {
   return (
     <div className="error-container">
-      <h1>500 - Server Error page not found</h1>
+      <h1>500 - Server Error </h1>
       <p>Oops! Something went wrong on our end.</p>
       <a href="/">Go back to homepage</a>
     </div>
